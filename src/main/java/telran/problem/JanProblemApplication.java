c10788cd4bc4a53717deffcdb4a95142193281b2
edward <edward@edward.com>
2023-08-06 12:46:59 +0300

    Initial commit

---
+++ src/main/java/telran/problem/JanProblemApplication.java
@@ -0,0 +1,13 @@
+package telran.problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanProblemApplication.class, args);
+	}
+
+}
