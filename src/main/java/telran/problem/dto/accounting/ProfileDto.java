7583099636f82e50b4c3c6b2e655eb10922db0ce
stas <rsl14591@gmail.com>
2023-08-27 08:24:11 +0300

    27.08.2023 Security repaired

--- src/main/java/telran/problem/dto/accounting/ProfileDto.java
+++ src/main/java/telran/problem/dto/accounting/ProfileDto.java
@@ -1,16 +1,10 @@
 package telran.problem.dto.accounting;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 import java.util.Map;
 import java.util.Set;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
 @Getter
 public class ProfileDto {
     protected String username;
@@ -18,27 +12,9 @@ public class ProfileDto {
     protected String educationLevel;
     protected Set<String> communities;
     protected LocationDto location;
-    protected String password;
     protected Set<String> roles;
     protected String avatar;
     protected StatsDto stats;
     protected Map<String, ActivityDto> activities;
     protected Double wallet;
-
-    public void addActivity(String id, ActivityDto activity) {
-        this.activities.put(id, activity);
-    }
-
-    public void removeActivity(String id) {
-        this.activities.remove(id);
-    }
-
-    public void addFormulatedProblem(){
-        this.stats.setFormulatedProblems(this.stats.getFormulatedProblems()+1);
-    }
-
-    public void removeFormulatedProblem(){
-        this.stats.setFormulatedProblems(this.stats.getFormulatedProblems()-1);
-    }
-
 }
